9a65597a3841cbed77793dd2746003c2edd8531a
happybajwa <happybajwa100@gmail.com>
2023-04-09 01:28:09 +1200

    Search-Box

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { SearchBox } from "@fluentui/react";
+import axios from "axios";
+import { initializeIcons } from '@fluentui/react/lib/Icons';
+
+
+initializeIcons();
 
 function App() {
+  const [users, setUsers] = useState<any[]>([]);
+  const [filteredData, setFilteredData] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.slingacademy.com/v1/sample-data/users?limit=20")
+      .then((response) => {
+        setUsers(response.data.users);
+        setFilteredData(response.data.users)
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleSearch = (event: any) => {
+    const searchTerm = event.target.value;
+    const filteredData = users.filter((user) => {
+      const phoneMatch = user.phone.includes(searchTerm);
+      return phoneMatch;
+    });
+    setFilteredData(filteredData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <SearchBox
+          style={{ maxWidth: "400px" }}
+          type="number"
+          placeholder="Enter Phone Number"
+          onChange={handleSearch}
+          onSearch={handleSearch}
+        />
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>State</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.first_name}</td>
+                <td>{user.phone}</td>
+                <td>{user.state}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
